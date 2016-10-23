b193a4fb32c6a374ef299417b0cc9cc86e5cf141
bigbadkip <leovanexxen@gmail.com>
2016-10-23 20:18:34 +0200

    upgraded buy objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 
 
 var units = 0;
-var items = [{name:"0", count:0},
-            {name:"1", count:0},
-            {name:"2", count:0},
-            {name:"3", count:0},
-            {name:"4", count:0},
-            {name:"5", count:0}];
+var items = [{name:"0", count:0, price:10},
+            {name:"1", count:0, price:100},
+            {name:"2", count:0, price:1000},
+            {name:"3", count:0, price:10000},
+            {name:"4", count:0, price:100000},
+            {name:"5", count:0, price:100000}];
 
 
 function gameLoop() {
@@ -25,10 +25,10 @@ function gameLoop() {
 gameLoop();
 
 
-function buy(itemNr,price){
+function buy(itemNr){
 
-    if(units >= price){
-        units = units-price;
+    if(units >= items[itemNr].price){
+        units = units-items[itemNr].price;
         items[itemNr].count++;
     }
 
