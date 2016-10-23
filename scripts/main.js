67270491e22f3792dad9c99cdbe77c858ab5c9f7
bigbadkip <leovanexxen@gmail.com>
2016-10-23 20:15:12 +0200

    Buy function

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,38 @@
 
-window.onload = function start() {
-    gameLoop();
-}
 
 var units = 0;
+var items = [{name:"0", count:0},
+            {name:"1", count:0},
+            {name:"2", count:0},
+            {name:"3", count:0},
+            {name:"4", count:0},
+            {name:"5", count:0}];
 
 
 function gameLoop() {
 
-//Add units   
-units = units+1;
+    //Add units   
+    units = units+1;
 
-//Update GUI
-$("#unitcounter").html("UNITS:" + units);
+    //Update GUI
+    $("#unitcounter").html("UNITS:" + units);
 
-//this must be the last statment
-setTimeout(gameLoop, 1000);
+    //this must be the last statment
+    setTimeout(gameLoop, 1000);
 }
 
-// boot up the first call
+//Start the game the first time
 gameLoop();
+
+
+function buy(itemNr,price){
+
+    if(units >= price){
+        units = units-price;
+        items[itemNr].count++;
+    }
+
+
+};
+
+
