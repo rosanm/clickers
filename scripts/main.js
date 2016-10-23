599a97d015fdecfed1e0e6e7b9684139bedb1969
bigbadkip <leovanexxen@gmail.com>
2016-10-23 20:21:16 +0200

    adds max amount to items

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 
 
 var units = 0;
-var items = [{name:"0", count:0, price:10},
-            {name:"1", count:0, price:100},
-            {name:"2", count:0, price:1000},
-            {name:"3", count:0, price:10000},
-            {name:"4", count:0, price:100000},
-            {name:"5", count:0, price:100000}];
+var items = [{name:"0", count:0, price:10, max:1},
+            {name:"1", count:0, price:100, max:100},
+            {name:"2", count:0, price:1000, max:100},
+            {name:"3", count:0, price:10000, max:200},
+            {name:"4", count:0, price:100000, max:500},
+            {name:"5", count:0, price:100000, max:10000}];
 
 
 function gameLoop() {
