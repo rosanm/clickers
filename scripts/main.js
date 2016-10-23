ccbd0edbb0e1e4af0e00aea568d2dae139238a69
bigbadkip <leovanexxen@gmail.com>
2016-10-23 20:47:48 +0200

    Populate the zoo!

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,34 @@
 
 
-var units = 0;
-var items = [{name:"0", count:0, price:10, gains:1, max:1},
-            {name:"1", count:0, price:100, gains:2, max:100},
-            {name:"2", count:0, price:1000, gains:5, max:100},
-            {name:"3", count:0, price:10000, gains:25, max:200},
-            {name:"4", count:0, price:100000, gains:50, max:500},
-            {name:"5", count:0, price:100000, gains:500, max:10000}];
+var units = 10000;
+var items = [{name:"Maginifying glass", count:0, price:10, gains:1, max:1},
+            {name:"Newspaper", count:0, price:100, gains:2, max:100},
+            {name:"Anonymous tip", count:0, price:1000, gains:5, max:100},
+            {name:"Watson", count:0, price:10000, gains:25, max:200},
+            {name:"Police dog", count:0, price:100000, gains:50, max:500},
+            {name:"Police station", count:0, price:100000, gains:500, max:10000}];
 
 
 function gameLoop() {
+var itemsString ="";
 
     //Add units 
         //Free 1ps  
         units += 1;
         //items
-        items.forEach(function(element) {
-            units += (element.count*element.gains);
+        items.forEach(function(e) {
+            units += (e.count*e.gains);
         }, this);
 
-    //Update GUI
-    $("#unitcounter").html("UNITS:" + units);
+        //Update GUI
+        $("#unitcounter").html("Pounds:" + units);
+
+        //Update ZOO
+        items.forEach(function(e) {
+                itemsString += e.name + ': ' + e.count +" <br/>";
+            }, this);
+
+        $(".theZoo").html(itemsString);
 
     //this must be the last statment
     setTimeout(gameLoop, 1000);
@@ -38,6 +46,8 @@ function buy(itemNr){
             //Buy it
             units = units-items[itemNr].price;
             items[itemNr].count++;
+        }else{
+            alert(items[itemNr].name +" is out of stock, maximum reached. " + items[itemNr].max +"/"+ items[itemNr].max )
         }
     }
 
