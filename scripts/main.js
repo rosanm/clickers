febf1c73412e6c62bbc9ff82aa60410e0641e03e
bigbadkip <leovanexxen@gmail.com>
2016-10-25 22:43:30 +0200

    Some lvl styling

--- scripts/main.js
+++ scripts/main.js
@@ -79,6 +79,8 @@ function calcUnitsPerSec(itemsArray){
 function updateGui(){
         $("#unitcounter").html("&pound;" + units );
         $("#unitpersec").html("PER SECOND:<br/>&pound; " + calcUnitsPerSec(items) );
+        $("#currentLvl").html(level);
+        currentLvl
         populateZoo();
         populateEnemy();
 };
