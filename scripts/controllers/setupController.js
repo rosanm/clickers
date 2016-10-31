33342243c86dd54f3d269178a779bb8943d513ba
rosanm <rosanmosterman@gmail.com>
2016-10-31 20:27:21 +0100

    refactoring populate enemy and attackenemy

--- scripts/controllers/setupController.js
+++ scripts/controllers/setupController.js
@@ -5,6 +5,8 @@ app.controller('setupController', function($scope) {
     $scope.units = 1000;
     $scope.ups = 0;
     $scope.level = 1;
+    $scope.currentEnemy = {};
+    $scope.door = "images/door.png";
     $scope.items = [{name:"Maginifying glass", img:"water.png",                count:0, price:10,     gains:1,     max:2},
                     {name:"Newspaper",          img:"water-glass.png",          count:0, price:100,    gains:2,     max:10},
                     {name:"Anonymous tip",      img:"water-glass-round.png",    count:0, price:500,    gains:10,    max:100},
@@ -12,12 +14,12 @@ app.controller('setupController', function($scope) {
                     {name:"Police dog",         img:"green-glass-round.png",    count:0, price:8000,   gains:500,   max:2},
                     {name:"Police station",     img:"pink-glass-round.png",     count:0, price:50000,  gains:2000,  max:2}];
 
-    $scope.enemys = [{name:"Dropphin", hp:1500,    total: 1500,    img:"007_dropphin_by_deoxysdaniel-d5j9slu.png"},
-                    {name:"Dolswim",    hp:5000,    total: 5000,    img:"008_dolswim_by_deoxysdaniel-d5jhd0v.png"},
-                    {name:"Arambly",    hp:20000,   total: 20000,   img:"034_arambly_by_deoxysdaniel-d5mriwg.png"},
-                    {name:"Umbrarach",  hp:100000,  total: 100000,  img:"035_umbrarach_by_deoxysdaniel-d5mx4t9.png"},
-                    {name:"Cubern",     hp:500000,  total: 500000,  img:"036_cubern_by_deoxysdaniel-d5n1gqm.png"},
-                    {name:"Gigarotto",  hp:2000000, total: 200000,  img:"037_gigarotto_by_deoxysdaniel-d5n1w4w.png"}]
+    $scope.enemys = [{name:"Dropphin", hp:1500,    total: 1500,    img:"images/enemys/007_dropphin_by_deoxysdaniel-d5j9slu.png"},
+                    {name:"Dolswim",    hp:5000,    total: 5000,    img:"images/enemys/008_dolswim_by_deoxysdaniel-d5jhd0v.png"},
+                    {name:"Arambly",    hp:20000,   total: 20000,   img:"images/enemys/034_arambly_by_deoxysdaniel-d5mriwg.png"},
+                    {name:"Umbrarach",  hp:100000,  total: 100000,  img:"images/enemys/035_umbrarach_by_deoxysdaniel-d5mx4t9.png"},
+                    {name:"Cubern",     hp:500000,  total: 500000,  img:"images/enemys/036_cubern_by_deoxysdaniel-d5n1gqm.png"},
+                    {name:"Gigarotto",  hp:2000000, total: 200000,  img:"images/enemys/037_gigarotto_by_deoxysdaniel-d5n1w4w.png"}]
 
     $scope.friends = [{name:"Pandoo", level:1500,    count:4, gains:100, img:"images/friends/001_pandoo_by_deoxysdaniel-d5j9po2.png", ups: 1},
                     {name:"Blazby",    level:5000,    count:2, gains:200, img:"images/friends/004_blazby_by_deoxysdaniel-d5j9qzc.png"},
@@ -38,32 +40,38 @@ app.controller('setupController', function($scope) {
 
     function populateEnemy() {
         var itemsString = "";    
-        e = $scope.enemys[$scope.level-1];                        
+        $scope.currentEnemy = $scope.enemys[$scope.level-1];       
+        $scope.$apply();                 
         var imgString ="";
         //Build image part                
-        imgString += '<img src="images/enemys/'+e.img+'" width="100%">';
+        //imgString += '<img src="images/enemys/'+e.img+'" width="100%">';
         //Build text part
-        itemsString += imgString + "<br/>" + e.name;  
+        //itemsString += imgString + "<br/>" + e.name;  
 
-        var progress = e.hp / e.total * 100;
+        var progress = $scope.currentEnemy.hp / $scope.currentEnemy.total * 100;
 
         $("progress").attr('value', progress);
 
-        $(".monster-box-enemy").html(itemsString);
+        //$(".monster-box-enemy").html(itemsString);
         $("#progresscontainer").show();
     };
 
     function attackEnemy() {
-    e = $scope.enemys[$scope.level-1];
-    e.hp = e.hp - $scope.ups;
-        if(e.hp < 0){
+        $scope.currentEnemy = $scope.enemys[$scope.level-1];
+        $scope.currentEnemy.hp = $scope.currentEnemy.hp - $scope.ups;
+        if($scope.currentEnemy.hp < 0){
             $scope.level = $scope.level + 1;
             $("#progresscontainer").hide();
-            setDoor();
+            $(".doorSet").show();
+            $(".enemySet").hide();
         }
         else {
+            $(".doorSet").hide();
+            $(".enemySet").show();
             populateEnemy();
         }
+
+        
     };
 
     function buy(itemNr){
@@ -97,10 +105,6 @@ app.controller('setupController', function($scope) {
         populateEnemy();
     };
 
-    function setDoor(){
-        var imgString = '<img id="door" src="images/door.png" width="100%">';
-        $(".monster-box-enemy").html(imgString);
-    }
 
     function gameLoop() {
             //update ups
@@ -135,10 +139,6 @@ app.controller('setupController', function($scope) {
    
     $(".section-center").resizable();
     $(".section-right").resizable();
-
-        //Build image part                
-        setDoor();
-
         $scope.items.forEach(function(i, index){      
             var div = $('<div/>',
             {
