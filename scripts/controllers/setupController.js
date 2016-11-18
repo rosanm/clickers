1bb44c60a505b4c788c0bb2dfb70e31193a3efc6
Rosan Mosterman <rosanmosterman@rosans-mbp.home>
2016-11-18 13:42:20 +0100

    fix door kick

--- scripts/controllers/setupController.js
+++ scripts/controllers/setupController.js
@@ -39,30 +39,30 @@ $(document).ready(function(){
                     $("#progresscontainer").show();
 
         //Deal DMG part
-        if(ractive.get('attack')){
-                  
-                    currentEnemy.hp = ractive.get('currentEnemy').hp - (ractive.get('dps')/100);
+        if(ractive.get('attack')) {                
+            currentEnemy.hp = ractive.get('currentEnemy').hp - (ractive.get('dps')/100);
 
-                    var progress = ractive.get('currentEnemy').hp / ractive.get('currentEnemy').total * 100;
-                    $("progress").attr('value', progress);
-                    $("#progresscontainer").show();
+            var progress = ractive.get('currentEnemy').hp / ractive.get('currentEnemy').total * 100;
+            $("progress").attr('value', progress);
+            $("#progresscontainer").show();
 
-                    ractive.update();
-                    
-                    if(ractive.get('currentEnemy.hp') < 0){
-                        ractive.set('doorSet', true);
-                        NextEnemy();
-                    }
-
+            ractive.update();
+            
+            if(ractive.get('currentEnemy.hp') < 0){
+                ractive.set('doorSet', true);
+                ractive.set('mine', true);
+                ractive.set('attack', false);
+            }
         }
-                //Control the attack/min variables
-                if(ractive.get('mine')){
-                    ractive.set('attack', false);
-                }
 
-                if(ractive.get('attack')){
-                    ractive.set('mine', false);
-                }
+        //Control the attack/min variables
+        if(ractive.get('mine')){
+            ractive.set('attack', false);
+        }
+
+        if(ractive.get('attack')){
+            ractive.set('mine', false);
+        }
 
         setTimeout(smoothAttackEnemy, 10); //loop 100x per sec  
     };
@@ -73,40 +73,31 @@ $(document).ready(function(){
         //verhoog level met 1
 
         if(!ractive.get('doorSet')){
+            ractive.set('level', ractive.get('level') + 1);
 
-                $(".doorSet").hide();
-                $(".enemySet").show()
-
-        ractive.set('level', ractive.get('level') + 1);
-
-            var currentEnemy = ractive.get('currentEnemy');
-        
-                var enemyIndex = ractive.get('enemyIndex');
-                if(enemyIndex == ractive.get('enemys').length - 1){           
-                    enemyIndex = 0;
-                }
-                else{
-                    enemyIndex = enemyIndex + 1;
-                }
-                    
-                var newEnemy = ractive.get('enemys')[enemyIndex];
-
-                //pak de volgende enemy met 61% meer hp als de vorige
-                newEnemy.hp = currentEnemy.total * 1.61803398875; //Golden Ratio;
-                newEnemy.total = newEnemy.hp;
-            
-
-                ractive.set('enemyIndex', enemyIndex);
-                ractive.set('currentEnemy', newEnemy);  
+            var currentEnemy = ractive.get('currentEnemy');        
+            var enemyIndex = ractive.get('enemyIndex');
+            if(enemyIndex == ractive.get('enemys').length - 1){           
+                enemyIndex = 0;
+            }
+            else {
+                enemyIndex = enemyIndex + 1;
+            }
                 
-                ractive.update();
+            var newEnemy = ractive.get('enemys')[enemyIndex];
 
-                var random = Math.floor((Math.random() * 250) + 1);
-                $('.enemySet').css('filter', 'hue-rotate(' + random + 'deg) saturate(3.3)');
+            //pak de volgende enemy met 61% meer hp als de vorige
+            newEnemy.hp = currentEnemy.total * 1.61803398875; //Golden Ratio;
+            newEnemy.total = newEnemy.hp;
+        
 
-        }else{
-                $(".doorSet").show();
-                $(".enemySet").hide()
+            ractive.set('enemyIndex', enemyIndex);
+            ractive.set('currentEnemy', newEnemy);  
+            
+            ractive.update();
+
+            var random = Math.floor((Math.random() * 250) + 1);
+            $('.enemySet').css('filter', 'hue-rotate(' + random + 'deg) saturate(3.3)');
         }
     };
 
@@ -233,7 +224,11 @@ $(document).ready(function(){
                 trainButton.isVisble = true;
             }
             ractive.update();
-        }
+        },
+        kickDoor: function(event) {
+            ractive.set('doorSet', false);
+            NextEnemy();
+        },
     });
 
     function hasEnoughDiamonds(amount){
@@ -292,9 +287,5 @@ $(document).ready(function(){
     gameLoop();
     //start the smoothscore
     smoothScoreLoop();
-
-            $( ".doorSet" ).click(function() {
-            ractive.set('doorSet', false);
-            });
 });
 
