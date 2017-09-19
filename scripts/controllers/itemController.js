8eea20c305709e7f684e01f2d3aafed9a9a4ccf2
bigbadkip <leovanexxen@gmail.com>
2017-09-19 21:04:01 +0200

    fix percentage up items

--- scripts/controllers/itemController.js
+++ scripts/controllers/itemController.js
@@ -7,18 +7,22 @@ var itemController = {
         //name of selectedFriend
         var selectedFriend = ractive.get('selectedFriend')(ractive.get('selectedFriendName'));
         var clickedItem = ractive.get(selectedFriend.itemListName)[itemNr];
-        //Can affound?
+        //Can afford?
         if(ractive.get('units') >= clickedItem.price) {
             //Still in stock?
             if(clickedItem.count < clickedItem.max){
                 //Buy it
                 ractive.set('units', ractive.get('units') - clickedItem.price);
-
+               
                 var upgradedItem = clickedItem;
                 upgradedItem.lvl = upgradedItem.lvl +1;
                 upgradedItem.price = Math.round(upgradedItem.price * 1.3);
                 upgradedItem.dmg = Math.round(upgradedItem.dmg * 1.2);
                 upgradedItem.count = upgradedItem.count + 1;
+
+                //als het een boost item is, de boost percentage erbij optellen
+                selectedFriend.boost = selectedFriend.boost + upgradedItem.boost;
+                
                 ractive.update();
             }
             else {
